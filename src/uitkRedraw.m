10be7b7ddd11a59b75aeb1fe0413dde4d7be4d97
Precise Simulation <18078865+precise-simulation@users.noreply.github.com>
2019-09-17 21:23:26 +0700

    GUITool 1.0 Build 19.09.260

---
+++ src/uitkRedraw.m
@@ -0,0 +1,10 @@
+function uitkRedraw( hFig, ~ )
+%UITKREDRAW Call figures nested uitk redrawing callbacks
+%
+%   UITKREDRAW( HFIG, ~ ) Forces an update and calling of nested uitk
+%   object ResizeFcn callbacks.
+%
+%   See also uitkRedrawGrid
+
+% Copyright 2013-2019 Precise Simulation, Ltd.
+
